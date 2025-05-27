7c9ee09b8f0fc36be2e7a49cc87d131a350bd073
nahomi2004 <nahomicabrera2004@gmail.com>
2025-05-27 09:15:56 -0500

    Consulta casi lista

--- consulta_02.py
+++ consulta_02.py
@@ -4,8 +4,26 @@
 	nombre del departamento y el número de cursos que tiene cada 
 	departamento
 '''
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_ # se importa el operador and
 
-from sqlalchemy import func
+# se importa la clase(s) del 
+# archivo genera_tablas
+from clases import Entrega, Estudiante, Curso, Departamento, Inscripcion
+
+import pandas as pd
+
+# se importa información del archivo configuracion
+from config import cadena_base_datos
+# se genera enlace al gestor de base de
+# datos
+# para el ejemplo se usa la base de datos
+# sqlite
+engine = create_engine(cadena_base_datos)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 departamentos_con_bajas_notas = session.query(
     Departamento.nombre,
