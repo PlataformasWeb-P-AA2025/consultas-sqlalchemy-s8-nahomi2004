f15b7ef2d85e4ec80852994ae4226bb38ccc0551
nahomi2004 <nahomicabrera2004@gmail.com>
2025-05-27 09:23:29 -0500

    Consulta 2 lista

--- consulta_02.py
+++ consulta_02.py
@@ -10,7 +10,7 @@ from sqlalchemy import and_ # se importa el operador and
 
 # se importa la clase(s) del 
 # archivo genera_tablas
-from clases import Entrega, Estudiante, Curso, Departamento, Inscripcion
+from clases import Entrega, Estudiante, Curso, Departamento, Inscripcion, Tarea
 
 import pandas as pd
 
@@ -25,11 +25,23 @@ engine = create_engine(cadena_base_datos)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Lo que obtengo es el nombre del departamento
+# Y el numero de cursos que cada uno tiene
+# trazo el camino con ayuda de join
+# filtro aquellos Departamentos que tengas entregas con 
+# notas mejores a 0.3
+# finalmente agrupo los departamentos 
 departamentos_con_bajas_notas = session.query(
-    Departamento.nombre,
-    func.count(Curso.id).label('cantidad_cursos')
-).join(Departamento.cursos).\
-    join(Curso.tareas).\
-    join(Tarea.entregas).\
-    filter(Entrega.calificacion <= 0.3).\
-    group_by(Departamento.id).all()
+                                    Departamento.nombre,
+                                    Entrega.calificacion,
+                                    func.count(Curso.id).label('cantidad_cursos')
+                                ).\
+                                join(Departamento.cursos).\
+                                join(Curso.tareas).\
+                                join(Tarea.entregas).\
+                                filter(Entrega.calificacion <= 0.3).\
+                                group_by(Departamento.id).all()
+
+for e in departamentos_con_bajas_notas:
+    print(e)
+    print("")
